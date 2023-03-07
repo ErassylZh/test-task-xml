1c8c69bc2f2c70ff1a45a62ed7a6bb4d70d9fa5c
Erosh01 <erasyl.zholdas@list.ru>
2023-03-07 22:06:04 +0600

    Delete create file methods Now service return only bytecode and add service interface

--- src/main/java/com/example/testtaskxml/controllers/CreditCardController.java
+++ src/main/java/com/example/testtaskxml/controllers/CreditCardController.java
@@ -1,20 +1,19 @@
 package com.example.testtaskxml.controllers;
 
 import com.example.testtaskxml.DTO.CreditCardDto;
-import com.example.testtaskxml.services.CreditCardService;
+import com.example.testtaskxml.services.impl.CreditCardServiceImpl;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/testxml")
 public class CreditCardController {
-    private final CreditCardService creditCardService;
+    private final CreditCardServiceImpl creditCardService;
 
-    public CreditCardController(CreditCardService creditCardService) {
+    public CreditCardController(CreditCardServiceImpl creditCardService) {
         this.creditCardService = creditCardService;
     }
 
---
+++ src/main/java/com/example/testtaskxml/services/impl/CreditCardServiceImpl.java
@@ -0,0 +1,238 @@
+package com.example.testtaskxml.services.impl;
+
+import com.example.testtaskxml.DTO.CreditCardDto;
+import com.example.testtaskxml.services.CreditCardService;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
+import java.util.Base64;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+@Service
+public class CreditCardServiceImpl implements CreditCardService {
+
+    private final JdbcTemplate jdbcTemplate;
+    public CreditCardServiceImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        {
+            try {
+                System.out.println(encodeZipFile());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public Document createXmlFile() throws Exception{
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);
+        Document doc = factory.newDocumentBuilder().newDocument();
+        Element root = doc.createElement("Records");
+        root.setAttribute("xmlns", "http://www.javacore.ru/schemas/");
+        doc.appendChild(root);
+        for (CreditCardDto card:getAllData()){
+            Element contract=doc.createElement("Contract");
+            contract.setAttribute("operation","1");
+
+            Element general=doc.createElement("General");
+            general.appendChild(getElementWithTextContext(doc,"ContractCode",card.getContractCode()));
+            general.appendChild(getElementWithTextContext(doc,"AgreementNumber",card.getAgreementNumber()));
+            general.appendChild(getElementWithAttribute(doc,"FundingType","id",card.getFundingType()));
+            general.appendChild(getElementWithAttribute(doc,"CreditPurpose2","id",card.getCreditPurpose2()));
+            general.appendChild(getElementWithAttribute(doc,"CreditObject","id",card.getCreditObject()));
+            general.appendChild(getElementWithAttribute(doc,"ContractPhase","id",card.getContractPhase()));
+            general.appendChild(getElementWithAttribute(doc,"ContractStatus","id",card.getContractStatus()));
+            general.appendChild(getElementWithTextContext(doc,"StartDate",card.getStartDate()));
+            general.appendChild(getElementWithTextContext(doc,"EndDate",card.getEndDate()));
+            general.appendChild(getElementWithTextContext(doc,"AnnualEffectiveRate",card.getAnnualEffectiveRate()));
+            general.appendChild(getElementWithTextContext(doc,"NominalRate",card.getNominalRate()));
+
+            Element subjects=doc.createElement("Subjects");
+
+            Element subject=doc.createElement("Subject");
+            subject.setAttribute("roleId","1");
+            Element entity=doc.createElement("Entity");
+            Element individual=doc.createElement("Individual");
+
+            Element firstName=doc.createElement("FirstName");
+            firstName.appendChild(getElementWithAttributeAndText(doc,"Text","language","ru-RU",card.getFirstname()));
+
+            Element surName=doc.createElement("SurName");
+            surName.appendChild(getElementWithAttributeAndText(doc,"Text","language","ru-RU",card.getSurname()));
+
+            Element fathersName=doc.createElement("FathersName");
+            fathersName.appendChild(getElementWithAttributeAndText(doc,"Text","language","ru-RU",card.getFathersname()));
+
+            individual.appendChild(firstName);
+            individual.appendChild(surName);
+            individual.appendChild(fathersName);
+            individual.appendChild(getElementWithAttribute(doc,"Classification","id","1"));
+            individual.appendChild(getElementWithAttribute(doc,"Residency","id",card.getResident()));
+            individual.appendChild(getElementWithTextContext(doc,"DateOfBirth",card.getDateOfBirth()));
+
+            Element identifications=doc.createElement("Identifications");
+            Element ident1=doc.createElement("Identification");
+            ident1.appendChild(getElementWithTextContext(doc,"Number",card.getIdentification1()));
+            ident1.appendChild(getElementWithTextContext(doc,"RegistrationDate",card.getIdent1_date_reg()));
+            Element ident2=doc.createElement("Identification");
+            ident2.appendChild(getElementWithTextContext(doc,"Number",card.getIdentification2()));
+            ident2.appendChild(getElementWithTextContext(doc,"RegistrationDate",card.getIdent2_date_reg()));
+            identifications.appendChild(ident1);
+            identifications.appendChild(ident2);
+            individual.appendChild(identifications);
+
+            Element addresses=doc.createElement("Addresses");
+            Element address1=doc.createElement("Address");
+            address1.setAttribute("katoId",card.getKatoid1());
+            Element streetName1=doc.createElement("StreetName");
+            streetName1.appendChild(getElementWithAttributeAndText(doc,"Text","language","ru-RU",card.getStreetName1()));
+            address1.appendChild(streetName1);
+
+
+            Element address2=doc.createElement("Address");
+            address1.setAttribute("katoId",card.getKatoid2());
+            Element streetName2=doc.createElement("StreetName");
+            streetName2.appendChild(getElementWithAttributeAndText(doc,"Text","language","ru-RU",card.getStreetName2()));
+            address2.appendChild(streetName2);
+
+            addresses.appendChild(address1);
+            addresses.appendChild(address2);
+
+            individual.appendChild(addresses);
+            entity.appendChild(individual);
+            subject.appendChild(entity);
+            subjects.appendChild(subject);
+            general.appendChild(subjects);
+
+            //-Type
+            Element type=doc.createElement("Type");
+            Element credit=getElementWithAttribute(doc,"Credit","paymentMethod","3");
+            Element creditLimit=getElementWithAttributeAndText(doc,"CreditLimit","currency","KZT","WHAT IS IT?");
+            Element records=doc.createElement("Records");
+
+            Element record=getElementWithAttribute(doc,"Record","accountingDate",card.getAccountingDate());
+            record.appendChild(getElementWithAttributeAndText(doc,"MonthlyInstalmentAmount","currency","KZT",card.getInstalmentAmount()));
+            record.appendChild(getElementWithAttributeAndText(doc,"ResidualAmount","currency","KZT","0"));
+
+            records.appendChild(record);
+            credit.appendChild(creditLimit);
+            credit.appendChild(records);
+            type.appendChild(credit);
+            contract.appendChild(general);
+            contract.appendChild(type);
+            root.appendChild(contract);
+        }
+
+        return doc;
+    }
+
+    private Element getElementWithAttributeAndText(Document doc,String tagName, String attributeName,String attributeValue,String contentText){
+        Element res=doc.createElement(tagName);
+        res.setAttribute(attributeName,attributeValue);
+        res.setTextContent(contentText);
+        return res;
+    }
+    private Element getElementWithAttribute(Document doc,String tagName, String attributeName,String attributeValue){
+        Element res=doc.createElement(tagName);
+        res.setAttribute(attributeName,attributeValue);
+        return res;
+    }
+    private Element getElementWithTextContext(Document doc,String tagName, String contentText){
+        Element res=doc.createElement(tagName);
+        res.setTextContent(contentText);
+        return res;
+    }
+
+    public ByteArrayOutputStream xmlToZipFile(){
+        try {
+            // Create a new Document object
+            Document document = createXmlFile();
+
+            // Create a new ZipOutputStream
+            ByteArrayOutputStream byteArrayOutputStream= new ByteArrayOutputStream();
+            ZipOutputStream zipOutputStream = new ZipOutputStream(byteArrayOutputStream);
+
+            // Create a new ZipEntry for the XML file
+            ZipEntry xmlEntry = new ZipEntry("cards.xml");
+            zipOutputStream.putNextEntry(xmlEntry);
+
+            // Transform the Document object to a Source object
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(document);
+
+            // Write the Source object to the ZipOutputStream
+            StreamResult result = new StreamResult(zipOutputStream);
+            transformer.transform(source, result);
+
+            // Close the ZipEntry and ZipOutputStream
+            zipOutputStream.closeEntry();
+            zipOutputStream.close();
+            return byteArrayOutputStream;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String encodeZipFile(){
+
+        byte[] zipBytes = xmlToZipFile().toByteArray();
+
+        // Encode the zip file contents using Base64
+        String base64Encoded = Base64.getEncoder().encodeToString(zipBytes);
+
+        return base64Encoded;
+    }
+
+
+    public List<CreditCardDto> getAllData(){
+        String query="SELECT * FROM fcb_daily_report_fields2";
+        return jdbcTemplate.query(query,(rs,rownum)->
+                    new CreditCardDto(
+                            rs.getString("contractCode"),
+                            rs.getString("agreementNumber"),
+                            rs.getString("findingType"),
+                            rs.getString("creditPurpose2"),
+                            rs.getString("creditObject"),
+                            rs.getString("contractPhrase"),
+                            rs.getString("contractStatus"),
+                            rs.getString("startDate"),
+                            rs.getString("endDate"),
+                            rs.getString("annualEffectiveRate"),
+                            rs.getString("nominalRate"),
+                            rs.getString("paymentPeriod"),
+                            rs.getString("totalAmount"),
+                            rs.getString("instalmentAmount"),
+                            rs.getString("outstanding_amount"),
+                            rs.getString("firstname"),
+                            rs.getString("surname"),
+                            rs.getString("fathersname"),
+                            rs.getString("dateOfBirth"),
+                            rs.getString("identification1"),
+                            rs.getString("ident1_date_reg"),
+                            rs.getString("identification2"),
+                            rs.getString("katoid1"),
+                            rs.getString("katoid2"),
+                            rs.getString("streetName1"),
+                            rs.getString("streetName2"),
+                            rs.getString("accountingDate"),
+                            rs.getString("ident2_date_reg"),
+                            rs.getString("resident"),
+                            rs.getString("fcb_status"),
+                            rs.getString("scb_status"),
+                            rs.getString("batch_fcb"),
+                            rs.getString("batch_scb")
+                    )
+                );
+    }
+}
